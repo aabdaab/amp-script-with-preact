4ab4bee0fcad6f827b45e8e2ebc5cc4c40b3b4d8
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-12-03 17:05:22 +0100

    Add CORS specifics

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(async (ctx, next) => {
   const origin = `${ctx.request.protocol}://${ctx.request.host}`
-  ctx.set('AMP-Access-Control-Allow-Source-Origin', origin)
+  ctx.set('AMP-Access-Control-Allow-Source-Origin', origin);
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
   await next()
 })
 
